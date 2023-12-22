46b4f321965c9064356d855b9fefc4b744691d16
Vikram Penumarti <vikram.penumarti@gmail.com>
2023-12-21 16:10:21 -0800

    Working code, projects page is still weird

--- vikram-penumarti/tailwind.config.ts
+++ vikram-penumarti/tailwind.config.ts
@@ -2,12 +2,9 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    // './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    // './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    // './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./lib/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/lib/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
